a62b02292a8e4dcfc88020649e8fe5f251493746
Michelle Hlcn <83108919+Michellehlcn@users.noreply.github.com>
2022-07-10 16:00:20 +1000

    update RSA

--- src/main/java/com/michellehlcn/orders/BotApplication.java
+++ src/main/java/com/michellehlcn/orders/BotApplication.java
@@ -8,6 +8,15 @@ public class BotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
+        
+        for(int i =0; i < 5; i++) {
+            System.out.println("It's so freaking cold outside!");
+        }
+
+        String[] colors = {"Red", "Blue", "Green"};
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(colors[i]);
+        }
     }
 }
-// SymphonyBdk bdk = new SymphonyBdk(loadFromFile("application.yaml"));
